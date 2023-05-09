1476f9ceecbeddc82d068f98e8902dfc4f823393
Mitya <88312178+mityalovescheese@users.noreply.github.com>
2023-05-09 15:40:00 -0700

    Update fitness.py

--- fitness.py
+++ fitness.py
@@ -5,17 +5,12 @@ class Fitness:
 		return result
 
 	def fitness(self, desired_result, current):
-		# recent_population_set is the three most recent nodes in the lineage. lineages are just the sublists that contain a set of different genomes evolving from the previous entity.
 		is_fit_to_live = False
 		distance_relative_to_previous = ""
 
-		# step 1: solve for the most recent equation and see how far off it is from the desired solution.
 		presolved_desired_result = self.solve_equation(desired_result)
 		presolved_current_result = self.solve_equation(current)
 
 		fitness_number = float(presolved_current_result) - float(presolved_desired_result)
 
 		return fitness_number
-		# ((recent_average + most_recent_node)/amount_of_added_numbers - desired_result)
-		# step 2: condition against previous nodes in the linage, and condition against the sibling node in order to determine fitness (not because it's what happens in the wild, but because it optimizes the process)
-
